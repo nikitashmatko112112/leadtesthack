7576c69813c044b807c45d83eecd4f0aff557c87
Вероника Шванская <veronikasvanskaa@MacBook-Air-Veronika.local>
2023-06-21 10:59:39 +0300

    change config

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,21 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
-module.exports = defineConfig({
+module.exports = {
+  testFiles: "**/*.spec.js",
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
+  env: {
+    browser: "chrome",
+  },
+  baseUrl: "https://example.com",
+  viewportWidth: 1280,
+  viewportHeight: 720,
   e2e: {
     baseUrl: "https://leadhack.ru",
-
-    setupNodeEvents(on, config) {
+    setupNodeEvents: function (on, config) {
       allureWriter(on, config);
       return config;
     },
   },
-});
+};
